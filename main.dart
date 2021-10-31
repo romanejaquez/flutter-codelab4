77b0a480fab145131303038f118ec99c8720944e
Roman Jaquez <romanejaquez@gmail.com>
2021-10-30 22:09:13 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -251,23 +251,79 @@ class DonutMainPage extends StatelessWidget {
           ),
           DonutFilterBar(),
           Expanded(
-            child: Container(
-              child: Consumer<DonutService>(
-                builder: (context, donutService, child) {
-                  return ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: donutService.filteredDonuts.length,
-                    itemBuilder: (context, index) {
-                      var donut = donutService.filteredDonuts[index];
-
-                      return DonutCard(donutInfo: donut);
-                    },
-                  );
-                },
-              )
-            ),
+            child: Consumer<DonutService>(
+              builder: (context, donutService, child) {
+                return DonutList(donuts: donutService.filteredDonuts);
+              }
+            )
           )
       ],
+    );
+  }
+}
+
+class DonutList extends StatefulWidget {
+  List<DonutModel>? donuts;
+
+  DonutList({ this.donuts });
+
+  @override
+  State<DonutList> createState() => _DonutListState();
+}
+
+class _DonutListState extends State<DonutList> with TickerProviderStateMixin {
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+  List<DonutModel> insertedItems = [];
+  //List<AnimationController>? controllers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    var future = Future(() {});
+    for (var i = 0; i < widget.donuts!.length; i++) {
+      // controllers!.add(AnimationController(
+      // duration: const Duration(milliseconds: 3000), vsync: this)
+      // ..forward());
+      future = future.then((_) {
+        return Future.delayed(const Duration(milliseconds: 125), () {
+          insertedItems.add(widget.donuts![i]);
+          _key.currentState!.insertItem(i);
+        });
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    //controllers!.forEach((element) { element.dispose(); });
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedList(
+      key: _key,
+      scrollDirection: Axis.horizontal,
+      initialItemCount: insertedItems.length,
+      itemBuilder: (context, index, animation) {
+        var donut = widget.donuts![index];
+        return SlideTransition(
+          position: Tween(
+            begin: const Offset(0.2, 0.0),
+            end: const Offset(0.0, 0.0),
+          )
+          .animate(CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeInOut
+          )),
+          child: FadeTransition(
+            opacity: Tween(begin: 0.0, end: 1.0)
+            .animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut)),
+            child: DonutCard(donutInfo: donut)
+          ),
+        );
+      },
     );
   }
 }
